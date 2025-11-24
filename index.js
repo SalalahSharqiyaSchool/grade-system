011b29404792e9cad4c74ef670ac46fd27ca990a
SalalahSharqiyaSchool <efmen2016@gmail.com>
2025-11-25 01:10:21 +0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function showGrades() {
     status.innerHTML = "جارٍ تحميل البيانات...";
 
     // ضع رابط raw لملف grades.json على GitHub هنا
-    const url = "const url = "https://raw.githubusercontent.com/SalalahSharqiyaSchool/grade-system/main/grades.json?time=" + Date.now();
+    const url = "const url = "https://raw.githubusercontent.com/SalalahSharqiyaSchool/grade-system/refs/heads/main/grades.json" + Date.now();
 " + Date.now();
 
     fetch(url)
