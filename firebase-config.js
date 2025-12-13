dd48d20ec255e2bb45692410d48033a166f05e67
SalalahSharqiyaSchool <efmen2016@gmail.com>
2025-12-13 14:57:01 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
+// ملف firebase-config.js (نسخة compat)
 const firebaseConfig = {
   apiKey: "AIzaSyCAgMFuqobOQTOc5WmfVuZfNHAIdvJ4Vwo",
   authDomain: "salalahschooldatabase.firebaseapp.com",
   projectId: "salalahschooldatabase",
-  storageBucket: "salalahschooldatabase.firebasestorage.app",
+  storageBucket: "salalahschooldatabase.appspot.com",
   messagingSenderId: "238841539748",
   appId: "1:238841539748:web:211e56160881c8c0f48a31",
   measurementId: "G-WTHY5MB9CF"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
